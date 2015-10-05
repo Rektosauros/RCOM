5650d687dad94c19329c65debcc6f0ccacd639d0
root <root@gnu34.netlab.fe.up.pt>
2015-10-05 18:19:17 +0100

    function added

---
+++ noncanonical.c
@@ -0,0 +1,121 @@
+/*Non-Canonical Input Processing*/
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define BAUDRATE B9600
+#define _POSIX_SOURCE 1 /* POSIX compliant source */
+#define FALSE 0
+#define TRUE 1
+
+#define A 0x03
+#define F 0x7e
+#define C_SET 0x07
+/*#define C_UA 
+#define BCC 
+*/
+
+volatile int STOP=FALSE;
+
+int main(int argc, char** argv)
+{
+    int fd,c, res;
+    struct termios oldtio,newtio;
+    char buf[255];
+
+    if ( (argc < 2) || 
+  	     ((strcmp("/dev/ttyS0", argv[1])!=0) && 
+  	      (strcmp("/dev/ttyS4", argv[1])!=0) )) {
+      printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
+      exit(1);
+    }
+
+
+  /*
+    Open serial port device for reading and writing and not as controlling tty
+    because we don't want to get killed if linenoise sends CTRL-C.
+  */
+  
+    
+    fd = open(argv[1], O_RDWR | O_NOCTTY );
+    if (fd <0) {perror(argv[1]); exit(-1); }
+
+    if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
+      perror("tcgetattr");
+      exit(-1);
+    }
+
+    bzero(&newtio, sizeof(newtio));
+    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+    newtio.c_iflag = IGNPAR;
+    newtio.c_oflag = 0;
+
+    /* set input mode (non-canonical, no echo,...) */
+    newtio.c_lflag = 0;
+
+    newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
+    newtio.c_cc[VMIN]     = 1;   /* blocking read until 5 chars received */
+
+
+
+  /* 
+    VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a 
+    leitura do(s) pr�ximo(s) caracter(es)
+  */
+
+
+
+    tcflush(fd, TCIOFLUSH);
+
+    if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
+      perror("tcsetattr");
+      exit(-1);
+    }
+
+    printf("New termios structure set\n");
+
+	unsigned char SET[5];
+	unsigned char UA[5];
+	sleep(2);
+
+	int i=0;
+	
+
+
+    while (STOP==FALSE) { 
+	if(i==4) STOP=TRUE;      
+      res = read(fd,buf,1);               
+	SET[i]=buf[0];
+	i++;
+    }
+
+	printf("Values read from SET: %X, %X, %X, %X, %X",SET[0],SET[1],SET[2],SET[3],SET[4]);
+
+	
+	
+
+	/*UA[0]=F;
+	UA[0]=A;
+	UA[0]=C_SET;
+	UA[0]=SET[1]^SET[2];
+	UA[0]=F;
+*/
+		
+
+  /* 
+    O ciclo WHILE deve ser alterado de modo a respeitar o indicado no gui�o 
+  */
+
+
+
+    tcsetattr(fd,TCSANOW,&oldtio);
+    close(fd);
+    return 0;
+}
+
